262dbe319a1530393526081d29c449ed79d7d8f6
Walter Pagani <paganiwalter@gmail.com>
2022-04-26 17:35:20 -0300

    (Script/Translations) Modifying texts into multiple languages

--- src/InstanceReset.cpp
+++ src/InstanceReset.cpp
@@ -5,9 +5,74 @@
 #include "GossipDef.h"
 #include "ScriptedGossip.h"
 #include "Language.h"
+#include "Chat.h"
+#include "WorldPacket.h"
+#include "WorldSession.h"
+#include "Opcodes.h"
 
 static bool instancereset_enable;
 static bool instancereset_normalmodeonly;
+
+void GossipSetText(Player* player, std::string message, uint32 textID)
+{
+    WorldPacket data(SMSG_NPC_TEXT_UPDATE, 100);
+    data << textID;
+    for (uint8 i = 0; i < MAX_GOSSIP_TEXT_OPTIONS; ++i)
+    {
+        data << float(0);
+        data << message;
+        data << message;
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+    }
+    player->GetSession()->SendPacket(&data);
+}
+
+class InstanceResetAnnouncer : public PlayerScript
+{
+public:
+    InstanceResetAnnouncer() : PlayerScript("InstanceResetAnnouncer") {}
+
+    void OnLogin(Player* player)
+    {
+        if (sConfigMgr->GetOption<bool>("instanceReset.Announcer", true))
+        {
+            std::string message = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                {
+                    message = "This server is running the |cff4CFF00Instance Reset |rmodule.";
+                    break;
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    message = "Este servidor está ejecutando el módulo |cff4CFF00Instance reset|r";
+                    break;
+                }
+                case LOCALE_ruRU:
+                {
+                    message = "Этот сервер использует модуль |cff4CFF00Перезапуск подземелий|r.";
+                    break;
+                }
+                default:
+                    break;
+            }
+            ChatHandler(player->GetSession()).SendSysMessage(message);
+        }
+    }
+};
 
 class instanceResetConfigLoad : public WorldScript {
 public:
@@ -29,7 +94,39 @@ public:
     {
         if (instancereset_enable) {
             ClearGossipMenuFor(player);
-            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "I would like to remove my instance saves.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            std::string gossipText = "";
+            std::string message = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                {
+                    gossipText = "I would like to remove my instance saves.";
+                    message = "Greetings $n. This is an npc that allows you to reset instance ids, allowing you to re-enter, without the need to wait for the reset time to expire. It was developed by the AzerothCore community.";
+                    break;
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    gossipText = "Me gustaría reiniciar mis ids de instancias.";
+                    message = "Saludos $n. Este es un npc que te permite reiniciar los ids de las instancias, permitiéndote volver a entrar, sin la necesidad de esperar a que se cumpla el tiempo para el reinicio. Fue desarrollado por la comunidad de AzerothCore.";
+                    break;
+                }
+                case LOCALE_ruRU:
+                {
+                    gossipText = "Я бы хотел удалить мои сохраненные подземелья.";
+                    message = "Приветствую, $n. Этот персонаж может удалить список посещенных подземелий, позволив Вам повторно их посетить не дожидаясь времени планового перезапуска. Разработан в AzerothCore community.";
+                    break;
+                }
+                default:
+                    break;
+            }
+            GossipSetText(player, message, DEFAULT_GOSSIP_MESSAGE);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, gossipText, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
         }
         return true;
@@ -57,7 +154,34 @@ public:
                         ++itr;
                 }
             }
-            creature->Whisper("Your instances have been reset.", LANG_UNIVERSAL, player);
+            std::string creatureWhisper = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                {
+                    creatureWhisper = "Your instances have been reset.";
+                    break;
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    creatureWhisper = "Sus instancias han sido restablecidas.";
+                    break;
+                }
+                case LOCALE_ruRU:
+                {
+                    creatureWhisper = "Ваши подземелья перезагружены.";
+                    break;
+                }
+                default:
+                    break;
+            }
+            creature->Whisper(creatureWhisper, LANG_UNIVERSAL, player);
             CloseGossipMenuFor(player);
         }
         return true;
@@ -67,5 +191,5 @@ public:
 void AddInstanceResetScripts() {
     new instanceResetConfigLoad();
     new instanceReset();
+    new InstanceResetAnnouncer();
 }
-
